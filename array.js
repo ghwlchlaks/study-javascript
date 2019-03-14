d26f55b11366e3a22c4fb94c9a667b6c2505c0b9
ghwlchlaks <ghwlchlaks@naver.com>
2019-03-14 22:16:45 +0900

    filter 메소드를 이용하여 배열 중복요소 필터링하기 공부

--- array.js
+++ array.js
@@ -9,7 +9,19 @@ foods[4] = 'e'
 
 console.log(foods);
 
+
+
+
 // 배열 중복 제거
 const arr  =  [1,1,3,4,5,1,4,2]
 const newArr = [...new Set(arr)]
 console.log(newArr)
+
+
+
+
+// 배열 유니크한 원소 뽑아내기 
+const arr1 = [1,2,3,4,4,2,1,5]
+// 중복되지 않는 값 가져오기(중복되는값 필터링)
+const newArr1 = arr1.filter(val => arr1.indexOf(val) === arr1.lastIndexOf(val))
+console.log(newArr1)
