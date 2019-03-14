02a572d52ffdc8ed632c97d2e66a321f3d3af126
ghwlchlaks <ghwlchlaks@naver.com>
2019-03-15 02:11:22 +0900

    typeof 공부

---
+++ type.js
@@ -0,0 +1,7 @@
+
+console.log(typeof null) //object
+console.log(typeof true) // boolean
+console.log(typeof function func() {}) // function
+console.log(typeof undefined) //undefined
+console.log(typeof 'test') // string
+console.log(typeof 123) //number
