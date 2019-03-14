5eb3cd29fc898580fc846c90a0a5718d7cc27a6f
ghwlchlaks <ghwlchlaks@naver.com>
2019-03-14 22:19:19 +0900

    filter 메소드를 이용하여 문자열 중복요소 필터링하기 공부

--- string.js
+++ string.js
@@ -3,3 +3,10 @@ const str = 'abbbaccde'
 const newStr = [...new Set(str)].join('')
 // join()연산을 하면 배열의 원소들은 연결하여 하나의 값으로 만듭니다.
 console.log(newStr)
+
+
+// 문자열 유니크한 원소 뽑아내기 
+const str1 = '23132115'
+// filter메소드를 이용하여 특정 요소 제거
+const newStr1 = str1.split('').filter(value => str1.indexOf(value) === str1.lastIndexOf(value))
+console.log(newStr1)
