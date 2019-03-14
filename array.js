560963e9dfd0ce776e56dfa3c34309b0116be52c
ghwlchlaks <ghwlchlaks@naver.com>
2019-03-14 22:28:47 +0900

    배열에서 특정 인덱스 요소 제거 공부(원본유지:slice, 원본훼손: splice

--- array.js
+++ array.js
@@ -10,7 +10,7 @@ foods[4] = 'e'
 console.log(foods);
 
 
-
+console.log('-----------------------------------------')
 
 // 배열 중복 제거
 const arr  =  [1,1,3,4,5,1,4,2]
@@ -18,10 +18,26 @@ const newArr = [...new Set(arr)]
 console.log(newArr)
 
 
-
+console.log('--------------------------------------------')
 
 // 배열 유니크한 원소 뽑아내기 
 const arr1 = [1,2,3,4,4,2,1,5]
 // 중복되지 않는 값 가져오기(중복되는값 필터링)
 const newArr1 = arr1.filter(val => arr1.indexOf(val) === arr1.lastIndexOf(val))
 console.log(newArr1)
+
+console.log('---------------------------------------------')
+
+// 배열 특정 인덱스 요소 제거
+const arr2 = [1,2,3,4,5]
+// slice(start, end) end의 요소는 적용안됨
+const newArr2 = [...arr2.slice(0,2), ...arr2.slice(3)]
+//원본유지
+console.log(arr2)
+console.log(newArr2)
+
+
+// 원본 훼손
+// splice(start, number) start 인덱스부터 number개의 인덱스를 삭제
+console.log(arr2.splice(2, 2))
+
