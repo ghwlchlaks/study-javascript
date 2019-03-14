6d282a19d2da96df40b28b47a71e0a1f1a31456c
ghwlchlaks <ghwlchlaks@naver.com>
2019-03-14 22:51:54 +0900

    문자-> 숫자, 숫자-> 문자 형변환 공부

--- string.js
+++ string.js
@@ -4,9 +4,28 @@ const newStr = [...new Set(str)].join('')
 // join()연산을 하면 배열의 원소들은 연결하여 하나의 값으로 만듭니다.
 console.log(newStr)
 
+console.log('-----------------------------------')
 
 // 문자열 유니크한 원소 뽑아내기 
 const str1 = '23132115'
 // filter메소드를 이용하여 특정 요소 제거
 const newStr1 = str1.split('').filter(value => str1.indexOf(value) === str1.lastIndexOf(value))
 console.log(newStr1)
+
+console.log('-----------------------------------')
+
+
+// 형변환
+// 문자 -> 숫자
+const str2 = '123';
+// + 붙혀주면 됨
+const strToNum = +str
+console.log(typeof strToNum);
+
+// 숫자 -> 문자
+const num = 123
+// 문자열인 '' 추가
+const numToStr = '' + num
+console.log(typeof numToStr)
+
+console.log('-------------------------------------')
